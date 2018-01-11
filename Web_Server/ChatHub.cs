3425084e653f276668acbcfb2dcc05dd47cd86d4
Mohanad Yousif <mohanad-yousif@outlook.com>
2018-01-11 18:29:53 +0100

    Add project files.

---
+++ Web_Server/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Web_Server
+{
+    public class ChatHub : Hub
+    {
+        public void SendMessage(string message, int color, string username)
+        {
+            Clients.All.UpdateChatMessage(message, color, username);
+        }
+    }
+}
